3ab912daaf4d3cd669eec9663bdfc7e7bcc6c5d4
Albert Samigullin (Akvelon INC) <v-alsami@microsoft.com>
2019-10-29 16:49:51 -0700

    Continue to work on min sum ASCII

--- leet/dp/minimum_sum_ACCII_from_two_sub_str.py
+++ leet/dp/minimum_sum_ACCII_from_two_sub_str.py
@@ -1,57 +1,32 @@
-def solution(s1, s2):
-    n = len(s1)
-    m = len(s2)
-    count = float('inf')
-    d = dict()
-    def find_min(s1, s2, n, m):
-        if n >= len(s1) - 1 or m >= len(s2)- 1:
-            return ""
-
-        key = str(n) + "|" + str(m)
-        if s1[n] == s2[m]:
-            if key in d:
-                #count = ord(s1[n]) + ord(s2[m])
-                print(d[key])
-                return d[key]
-            else:
-                d[key] = find_min(s1, s2, n + 1, m + 1) + s1[n]
-                print(d[key])
-                return d[key]
-        else:
-            if key in d:
-                #count = ord(s1[n]) + ord(s2[m])
-                print(d[key])
-                return d[key]
-            else:
-                a = find_min(s1, s2, n, m + 1)
-                print(a)
-                b = find_min(s1, s2, n + 1, m)
-                print(b)
-                d[key] = a if len(a)>len(b) else b
-                return d[key]
-
-
-    find_min(s1,s2, 0, 0)
-    print(d)
-
-
-def LCSLength (s1, s2):
+def LCSLength(s1, s2):
     l = [[0 for i in range(8)] for j in range(8)]
-    #print(l)
+    # print(l)
     for i in range(1, len(s1) + 1):
         for j in range(1, len(s2) + 1):
             if s1[i - 1] == s2[j - 1]:
-                l[i][j] = l[i-1][j-1] + 1
+                l[i][j] = l[i - 1][j - 1] + 1
             else:
-                l[i][j] = max(l[i-1][j], l[i][j-1])
-    print(l)
+                l[i][j] = max(l[i - 1][j], l[i][j - 1])
+    return l
+
+
+global lookup
+lookup = []
+
+def LCS(s1, s2, n, m, ):
+    if n == 0 and m == 0:
+        return ""
+    if s1[n-1] == s2[m-1]:
+        seq_list = LCS(s1, s2, n-1, m-1)
+
+        for s in seq_list:
+
+
 
 
 if __name__ == "__main__":
-    LCSLength("delete", "leet")
-    solution("delete", "leet")
-
-
-
-
-
+    s1 = "delete"
+    s2 = "leet"
+    lookup = LCSLength(s1, s2)
+    print(LCS(s1, s2, len(s1), len(s2)))
+    #solution("delete", "leet")
