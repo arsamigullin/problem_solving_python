f4d3d68270ded13af334442a3dbdfe74648f508f
Albert Samigullin (Akvelon INC) <v-alsami@microsoft.com>
2019-10-31 17:07:29 -0700

    added min sum ASCII

--- leet/dp/minimum_sum_ACCII_from_two_sub_str.py
+++ leet/dp/minimum_sum_ACCII_from_two_sub_str.py
@@ -1,32 +1,20 @@
-def LCSLength(s1, s2):
-    l = [[0 for i in range(8)] for j in range(8)]
-    # print(l)
-    for i in range(1, len(s1) + 1):
-        for j in range(1, len(s2) + 1):
-            if s1[i - 1] == s2[j - 1]:
-                l[i][j] = l[i - 1][j - 1] + 1
+def minimumDeleteSum(s1, s2):
+    dp = [[0] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+
+    for i in range(len(s1) - 1, -1, -1):
+        dp[i][len(s2)] = dp[i + 1][len(s2)] + ord(s1[i])
+    for j in range(len(s2) - 1, -1, -1):
+        dp[len(s1)][j] = dp[len(s1)][j + 1] + ord(s2[j])
+
+    for i in range(len(s1) - 1, -1, -1):
+        for j in range(len(s2) - 1, -1, -1):
+            if s1[i] == s2[j]:
+                dp[i][j] = dp[i + 1][j + 1]
             else:
-                l[i][j] = max(l[i - 1][j], l[i][j - 1])
-    return l
+                dp[i][j] = min(dp[i + 1][j] + ord(s1[i]),
+                               dp[i][j + 1] + ord(s2[j]))
 
-
-global lookup
-lookup = []
-
-def LCS(s1, s2, n, m, ):
-    if n == 0 and m == 0:
-        return ""
-    if s1[n-1] == s2[m-1]:
-        seq_list = LCS(s1, s2, n-1, m-1)
-
-        for s in seq_list:
-
-
-
+    return dp[0][0]
 
 if __name__ == "__main__":
-    s1 = "delete"
-    s2 = "leet"
-    lookup = LCSLength(s1, s2)
-    print(LCS(s1, s2, len(s1), len(s2)))
-    #solution("delete", "leet")
+    print(minimumDeleteSum("delete", "leet"))
