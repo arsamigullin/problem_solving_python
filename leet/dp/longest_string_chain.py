956a7a9922a44f0b90340753ca1850561cfd908b
Albert Samigullin (Akvelon INC) <v-alsami@microsoft.com>
2019-11-11 09:40:00 -0800

    longest string chain

--- leet/dp/longest_string_chain.py
+++ leet/dp/longest_string_chain.py
@@ -22,7 +22,28 @@ def solution(A):
     return find(1, A[0]) + 1
 
 
+def longestStrChain(words):
+    all_words = sorted(words, key=lambda x: len(x))
+
+    record = {}
+
+    dp = [1] * len(all_words)
+
+    for idx1, i in enumerate(all_words):
+
+        for j in range(len(i)):
+
+            curr_word = i[:j] + i[j + 1:]
+
+            if curr_word in record:
+                dp[idx1] = max(dp[idx1], dp[record[curr_word]] + 1)
+
+        record[i] = idx1
+
+    return max(dp)
+
 if __name__ == "__main__":
+    longestStrChain(longestStrChain(["a","b","ba","bca","bda","bdca"]))
     print(solution(["a","b","ba","bca","bda","bdca"]))
     s = ["czgxmxrpx","lgh","bj","cheheex","jnzlxgh","nzlgh","ltxdoxc","bju","srxoatl","bbadhiju","cmpx","xi","ntxbzdr","cheheevx","bdju","sra","getqgxi","geqxi","hheex","ltxdc","nzlxgh","pjnzlxgh","e","bbadhju","cmxrpx","gh","pjnzlxghe","oqlt","x","sarxoatl","ee","bbadju","lxdc","geqgxi","oqltu","heex","oql","eex","bbdju","ntxubzdr","sroa","cxmxrpx","cmrpx","ltxdoc","g","cgxmxrpx","nlgh","sroat","sroatl","fcheheevx","gxi","gqxi","heheex"]
     print(solution(sorted(s, key=lambda x: len(x), reverse=True)))
