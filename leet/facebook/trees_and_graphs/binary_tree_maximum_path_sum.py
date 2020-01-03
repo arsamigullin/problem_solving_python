00517a65295c7f737cd415529c8a917043893700
Albert Samigullin <ar.smglln@gmail.com>
2020-01-02 17:41:52 -0800

    tech debt

--- leet/facebook/trees_and_graphs/binary_tree_maximum_path_sum.py
+++ leet/facebook/trees_and_graphs/binary_tree_maximum_path_sum.py
@@ -1,3 +1,16 @@
+
+# this algo uses PostOrder traversal
+# at the root node we check if current sum of tree (made of current node being a root and its left and right children)
+# has maximum value by updating global max_sum variable
+# But since we look for path we must return node.val + max(node.left.val, node.right.val)
+# let's consider the tree
+#         -5
+#         /\
+#       -1 -6
+#       /\
+#     -2  -3
+# First we will see if -1 +
+
 class Solution:
     def maxPathSum(self, root):
         """
@@ -10,18 +23,21 @@ class Solution:
             if not node:
                 return 0
 
-            # max sum on the left and right sub-trees of node
+            # we compare here with 0 because if node and node.left are negative
+            # their sum will move away from maximum value
+            # there fore we does not count the negative numbers
+            # and current node will have max val
             left_gain = max(max_gain(node.left), 0)
             right_gain = max(max_gain(node.right), 0)
 
-            # the price to start a new path where `node` is a highest node
+            # obtain max val from current tree
             price_newpath = node.val + left_gain + right_gain
 
-            # update max_sum if it's better to start a new path
+            # check if the current tree has max val
             max_sum = max(max_sum, price_newpath)
 
-            # for recursion :
-            # return the max gain if continue the same path
+            # since we look for path (NOT Tree) we cannot return sum of tree
+            # we have to return sum of path
             return node.val + max(left_gain, right_gain)
 
         max_sum = float('-inf')
---
+++ leet/google/dp/jump_game.py
@@ -0,0 +1,56 @@
+# https://leetcode.com/problems/jump-game/
+# U - unknown (we didn't visited it yet)
+# G - good (We call a position in the array a "good index" if starting at that position, we can reach the last index.)
+# B - bad (that index is called a "bad index". The problem then reduces to whether or not index 0 is a "good index".)
+
+#Algo
+# just a reminder. At each position we have a number we can jump from current position
+# let say we have [1,2,3,4] array. At index 0 we can jump at most to 0+2 index
+#Algo
+# 1. init memo array with the same length as nums and fill it with U
+# 2. Starting from 0 index identify the max index you can jump to
+# and iterate over this distance starting from position + 1
+# 3. if the last index is reachable from the current index this current is going to be good
+
+#This is the inefficient solution where we try every single jump pattern that takes us from
+# the first position to the last. We start from the first position and jump to every index that
+# is reachable. We repeat the process until last index is reached. When stuck, backtrack.
+
+#TLE
+# Top-down approach
+class Solution:
+    def canJump(self, nums: list) -> bool:
+        memo = ['U'] * len(nums)
+        memo[-1] = 'G'
+        def canjmp(cur_index, nums):
+            if memo[cur_index]!='U':
+                return  memo[cur_index] == 'G'
+            max_distance_to_jump_from_cur_index = cur_index + nums[cur_index]
+            for i in range(cur_index + 1, max_distance_to_jump_from_cur_index + 1):
+                if canjmp(i, nums):
+                    memo[i] = 'G'
+                    return True
+            memo[i] = 'B'
+            return False
+        return canjmp(0, nums)
+
+if __name__ == "__main__":
+    s = Solution()
+    s.canJump([2,3,1,1,4])
+
+# In bottom-up approach we start from the end of array
+# TLE
+class Solution:
+    def canJump(self, nums):
+        memo = ['U'] * len(nums)
+        memo[-1] = 'G'
+        for i in range(len(nums) - 2, -1 , -1):
+            max_distance_to_jump = i + nums[i]
+            for j in range(i+1, max_distance_to_jump + 1):
+                if memo[j] == 'G':
+                    memo[i] = 'G'
+                    break
+        return memo[0] == 'G'
+
+# Accepted
+# Greedy
